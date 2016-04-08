3d218846d4daef3085adc278f1727cc7b209d4d1
darkomega9408 <darkomega9408@yahoo.com>
2016-04-08 10:21:52 +0700

    - Update home fragment: Continue slider when fragment is resume

--- Assignment/Source/BKara/app/src/main/java/com/beast/bkara/fragments/HomeFragment.java
+++ Assignment/Source/BKara/app/src/main/java/com/beast/bkara/fragments/HomeFragment.java
@@ -129,6 +129,12 @@ public class HomeFragment extends Fragment {
         super.onStop();
     }
 
+    @Override
+    public void onResume() {
+        sliderLayout.startAutoCycle();
+        super.onResume();
+    }
+
     /**
      * This interface must be implemented by activities that contain this
      * fragment to allow an interaction in this fragment to be communicated
